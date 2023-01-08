b9b8a4420f5fd2b64735595bd2d35a09d7d65bd3
Nic Annau <nicannau@gmail.com>
2023-01-07 23:58:57 -0800

    Flake 8

--- src/scripts/Annau-2022/experiment.py
+++ src/scripts/Annau-2022/experiment.py
@@ -7,6 +7,7 @@ from typing import Tuple
 import mlflow
 import torch
 
+
 @dataclass
 class SRModelRun:
     """Class for defining attributes of GAN model runs saved in MLFlow"""
@@ -16,7 +17,6 @@ class SRModelRun:
     parent_path: str
     generator_param_dim: Tuple[int, int, int, int] = (16, 128, 7, 2)
     critic_param_dim: Tuple[int, int, int] = (16, 128, 2)
-
 
     def _check_cuda_available(self):
         if not torch.cuda.is_available():
