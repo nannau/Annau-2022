94682aadc3042d032278a0b2dde7cf4c35d84af9
Nic Annau <nannau@uvic.ca>
2023-01-07 23:38:58 -0800

    Add initial run class

---
+++ src/scripts/Annau-2022/experiment.py
@@ -0,0 +1,32 @@
+from generator import Generator
+from critic import Critic
+
+from dataclasses import dataclass
+from typing import Tuple
+
+import mlflow
+
+@dataclass
+class SRModelRun:
+    """Class for defining attributes of GAN model runs saved in MLFlow"""
+    region: str
+    sr_config: str
+    exp_id: str
+    parent_path: str
+    generator_param_dim: Tuple[int, int, int, int] = (16, 128, 7, 2)
+    critic_param_dim: Tuple[int, int, int] = (16, 128, 2)
+
+    def load_generator(self):
+        G = Generator(*self.generator_param_dim).cuda()
+        return self._load_state_dict("Generator", G)
+
+    def load_critic(self):
+        C = Critic(*self.critic_param_dim).cuda()
+        return self._load_state_dict("Critic", C)
+
+    def _load_state_dict(self, model_name, model_class):
+        state_dict = mlflow.pytorch.load_state_dict(
+            self.parent_path + self.exp_id + "/" + model_name
+        )
+        model_class.load_state_dict(state_dict)
+        return model_class
