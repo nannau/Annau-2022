23443060b2e9d82fc21775fca309ac4e640bd34b
Nic Annau <nannau@uvic.ca>
2023-01-22 22:52:27 -0800

    Calculate over proper dimensions. Fix generator bug

--- Annau2022/RAPSD.py
+++ Annau2022/RAPSD.py
@@ -11,10 +11,10 @@ radially averaged power spectral density (RASPD)
 
 def calculate_2dft(image: np.ndarray) -> np.ndarray:
     """Computes the fourier transform and returns the amplitudes"""
-    # fourier_image = np.fft.fftn(image)
-    fourier_image = torch.fft.fftn(image)
-    # fourier_amplitudes = np.abs(fourier_image)**2
-    fourier_amplitudes = torch.abs(fourier_image)**2
+    fourier_image = np.fft.fftn(image)
+    # fourier_image = torch.fft.fftn(image)
+    fourier_amplitudes = np.abs(fourier_image)**2
+    # fourier_amplitudes = torch.abs(fourier_image)**2
 
     return fourier_amplitudes.flatten()
 
@@ -35,18 +35,16 @@ def compute_rapsd(hr_field: Generator, var_ref: dict = None) -> np.ndarray:
 
     var_rapsd = {}
     [var_rapsd.setdefault(x, []) for x in var_ref]
-    var_rapsd_avg = {}
-
 
     for x in hr_field:
-        knrm = define_wavenumers(x.shape[-1])
-        # Bins to average the spectra over
-        kbins = np.arange(0.5, x.shape[-1]//2+1, 1.)
-        # "Interpolate" at the bin centers
-        kvals = 0.5 * (kbins[1:] + kbins[:-1])
-
         for var_name, var_idx in var_ref.items():
-            wind_2d = calculate_2dft(x[0, var_idx]).cpu().detach().numpy()
+
+            knrm = define_wavenumers(x.shape[-1])
+            # Bins to average the spectra over
+            kbins = np.arange(0.5, x.shape[-1]//2+1, 1.)
+            # "Interpolate" at the bin centers
+            kvals = 0.5 * (kbins[1:] + kbins[:-1])
+            wind_2d = calculate_2dft(x[var_idx, ...].cpu().detach().numpy())
 
             # This ends up compute the radial average
             average_bins, _, _ = stats.binned_statistic(
@@ -58,12 +56,9 @@ def compute_rapsd(hr_field: Generator, var_ref: dict = None) -> np.ndarray:
             # Add to a list -- each element is a RASPD
             var_rapsd[var_name].append(average_bins)
 
-        var_rapsd_avg["k"] = kvals
-
-    for key in var_ref:
-        var_rapsd_avg[key] = np.mean(np.array(var_rapsd[key]), axis=0)
-        # var_rapsd_avg[key] = np
-    # [var_rapsd_avg.setdefault(key, np.mean(np.array(var_rapsd[key]), axis=0)) for key in var_names]
-
+    var_rapsd_avg = {}
+    var_rapsd_avg["k"] = kvals
+    for var_name in var_ref:
+        var_rapsd_avg[var_name] = np.mean(np.array(var_rapsd[var_name]), axis=0)
 
     return var_rapsd_avg
