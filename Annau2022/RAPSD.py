6b5f2594247bdb5e0e86cdc62131587e2c3f6fd2
Nic Annau <nannau@uvic.ca>
2023-01-27 14:41:40 -0800

    Factor out mean bins -> new func

--- Annau2022/RAPSD.py
+++ Annau2022/RAPSD.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Tuple
 
 import numpy as np
 import scipy.stats as stats
@@ -27,7 +27,23 @@ def define_wavenumers(hr_dim: int) -> np.ndarray:
     return knrm.flatten()
 
 
-def compute_rapsd(hr_field: Generator, var_ref: dict = None) -> np.ndarray:
+def get_mean_bins(x, var_idx, knrm) -> Tuple:
+    """Calculates the mean bins for the RASPD"""
+    kbins = np.arange(0.5, x.shape[-1]//2+1, 1.)
+    kvals = 0.5 * (kbins[1:] + kbins[:-1])
+    wind_2d = calculate_2dft(x[var_idx, ...].cpu().detach().numpy())
+    average_bins, _, _ = stats.binned_statistic(
+        knrm, wind_2d, statistic="mean", bins=kbins
+        )
+    average_bins *= np.pi * (kbins[1:]**2 - kbins[:-1]**2)
+    return average_bins, kvals
+
+
+def compute_rapsd(
+        hr_field: Generator,
+        var_ref: dict = None,
+        reduce=True
+) -> np.ndarray:
     """Computes the RASPD for a given super-resolution model"""
 
     if var_ref is None:
@@ -38,29 +54,18 @@ def compute_rapsd(hr_field: Generator, var_ref: dict = None) -> np.ndarray:
 
     for x in hr_field:
         for var_name, var_idx in var_ref.items():
-
             knrm = define_wavenumers(x.shape[-1])
-            # Bins to average the spectra over
-            kbins = np.arange(0.5, x.shape[-1]//2+1, 1.)
-            # "Interpolate" at the bin centers
-            kvals = 0.5 * (kbins[1:] + kbins[:-1])
-            wind_2d = calculate_2dft(x[var_idx, ...].cpu().detach().numpy())
-
-            # This ends up compute the radial average
-            average_bins, _, _ = stats.binned_statistic(
-                knrm, wind_2d, statistic="mean", bins=kbins
-                )
-
-            # Weight by the surface area in radial average bin
-            average_bins *= np.pi * (kbins[1:]**2 - kbins[:-1]**2)
-            # Add to a list -- each element is a RASPD
+            average_bins, kvals = get_mean_bins(x, var_idx, knrm)
             var_rapsd[var_name].append(average_bins)
 
     var_rapsd_avg = {"k": kvals}
     for var_name in var_ref:
-        var_rapsd_avg[var_name] = np.mean(
-            np.array(var_rapsd[var_name]),
-            axis=0
-        )
+        if reduce:
+            var_rapsd_avg[var_name] = np.mean(
+                np.array(var_rapsd[var_name]),
+                axis=0
+            )
+        else:
+            var_rapsd_avg[var_name] = var_rapsd[var_name]
 
     return var_rapsd_avg
