7233e46344cbeddae4711f531bdb639aa6cc1a95
Nic Annau <nannau@uvic.ca>
2023-02-03 15:30:26 -0800

    Gets env pass for Jupyter

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,9 +1,10 @@
+import os
 from notebook.auth import passwd
 
 # Note this is NOT a secure password. I include this
 # just for ease of use. Please change this to be secure if
 # you are running a public facing service
-my_password = "annau2023"
+my_password = os.environ["JUPYTER_PASSWORD"]
 hashed_password = passwd(passphrase=my_password, algorithm='sha256')
 
 c.NotebookApp.ip = '0.0.0.0'
